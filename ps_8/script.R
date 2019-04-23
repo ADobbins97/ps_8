3816769542577a840a039d04395b07f15e142c51
Mike Silva <mikesilva1@Mikes-MacBook-Air-3.local>
2019-04-22 22:04:52 -0400

    ahhhh

--- ps_8/script.R
+++ ps_8/script.R
@@ -7,6 +7,7 @@ library(tidycensus)
 library(leaflet)
 library(mapview)
 library(tigris)
+library(lubridate)
 
 census_api_key("a04411e6531c865e7b27d166476949e65577d6cd", install = TRUE)
 
@@ -46,24 +47,28 @@ shapes <-
 
 st_crs(shapes)
 
-st_as_sf(wilmington,
+penis <- 
+  st_as_sf(wilmington,
          coords = c("longitude", "latitude"),
          crs = 4269) 
 
 wilmington_map <- 
   ggplot(data = shapes) +
-  geom_sf()
+  geom_sf(data = shapes)
 
 wilmington_map
 
 
-wilmington <-
+wilmington_points <-
   wilmington %>% 
   select(first_unit_there, last_unit_to_leave_the_scene, latitude, longitude) %>% 
   filter(first_unit_there != "NULL", last_unit_to_leave_the_scene != "NULL", latitude != "NA", longitude != "NA") %>% 
   #strsplit(format(first_unit_there, "%d %m %Y %H:%M"), ' ') 
-  mutate(first_unit_there = mdy_hm(first_unit_there)) %>% 
-  mutate(last_unit_to_leave_the_scene = mdy_hm(last_unit_to_leave_the_scene)) 
+  mutate(first_unit_there = parse_date_time(first_unit_there, "%d %%m %Y %H:%M %S", truncated = 3)) %>%
+  #mutate(first_unit_there = dmy_hm(first_unit_there)) %>% 
+  #mutate(last_unit_to_leave_the_scene = mdy_hm(last_unit_to_leave_the_scene)) 
+  mutate(last_unit_to_leave_the_scene = parse_date_time(last_unit_to_leave_the_scene, "%d %%m %Y %H:%M %S", truncated = 3))
+
 
 #Comparison of duration of time at scene for 
 
